c78576e9a240eaa154c4113d419149dddfa8dfb7
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2022-06-03 13:29:08 +0100

    :construction: neede to fix SyntaxHighlighter element

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -8,20 +8,40 @@ import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
 import toc from "@jsdevtools/rehype-toc";
 
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+import Image from "next/image";
+
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
 
 export default function Post({ frontmatter, content }: any) {
   const { title, author, category, date, tags } = frontmatter;
 
+  const postDate = new Date(date);
   return (
     <Layout>
       <h1>{title}</h1>
       <h2>
-        {author} || {date}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={`https://github.com/${author}`}
+        >
+          {/* <Image
+            src={`https://github.com/${author}.png`}
+            alt="Picture of author"
+            width={50}
+            height={50}
+          /> */}
+          {author}
+        </a>
+        || {postDate.toDateString()}
       </h2>
       <h3>
-        {/* {category
+        {/* TODO: Add Link to category
+         {category
           ? category.map((cat: string) => {
               return <span key={cat}>{`${cat} `}</span>;
             })
@@ -29,12 +49,36 @@ export default function Post({ frontmatter, content }: any) {
         {category.join(", ")}
         || {tags.join(", ")}
       </h3>
-      <ReactMarkdown
+      {/* <ReactMarkdown
         remarkPlugins={[remarkGfm, remarkBreaks]}
         rehypePlugins={[rehypeHighlight, rehypeRaw, rehypeSanitize, toc]}
       >
         {content}
-      </ReactMarkdown>
+      </ReactMarkdown> */}
+
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkBreaks]}
+        rehypePlugins={[rehypeRaw, toc]}
+        children={content}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                // TODO: fix typescript error
+                children={String(children).replace(/\n$/, "")}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
     </Layout>
   );
 }
