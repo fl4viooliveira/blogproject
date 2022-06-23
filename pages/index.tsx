3ce7672f676333c273bbc048068d353ece5d0b51
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2022-06-23 11:15:37 +0100

    :mag: Add Tags to improve the SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import fs from "fs";
 import matter from "gray-matter";
 import Link from "next/link";
+import Head from "next/head";
 import { GetStaticProps } from "next";
 import styles from "../styles/BlogHome.module.css";
 import Layout from "../components/Layout";
@@ -16,6 +17,31 @@ export default function BlogHomePage({ posts }: any) {
 
   return (
     <Layout>
+      <Head>
+        {/* Primary Meta Tags  */}
+        <title>BlogProject.io</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="BlogProject.io" key="BlogProject.io" />
+        <meta name="description" content="Here's a project that can be valuable for developers who want to have a blog simply and most cheaply. Deploying on Vercel, it's free for hobby accounts.
+This blog project doesn't use a database, and the posts are Markdown files." />
+        <meta name="keywords" content="javascript, webdev, frontend, backend, reactjs, nextjs, nodejs, opensource, blog, devblog," />
+
+        {/* Open Graph / Facebook  */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://blogproject.io/" />
+        <meta property="og:title" content="BlogProject.io" />
+        <meta property="og:description" content="Here's a project that can be valuable for developers who want to have a blog simply and most cheaply. Deploying on Vercel, it's free for hobby accounts.
+This blog project doesn't use a database, and the posts are Markdown files." />
+        <meta property="og:image" content="https://blogproject.io/img-tag.png" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://blogproject.io/" />
+        <meta property="twitter:title" content="BlogProject.io" />
+        <meta property="twitter:description" content="Here's a project that can be valuable for developers who want to have a blog simply and most cheaply. Deploying on Vercel, it's free for hobby accounts.
+This blog project doesn't use a database, and the posts are Markdown files." />
+        <meta property="twitter:image" content="https://blogproject.io/img-tag.png" />
+      </Head>
       <Banner />
       <main className={styles.mainBox}>
         {posts.map((post: any) => {
@@ -50,13 +76,13 @@ export default function BlogHomePage({ posts }: any) {
               <p>
                 {category
                   ? category.map((cat: string) => {
-                      return (
-                        <span
-                          className={styles.category}
-                          key={cat}
-                        >{`${cat}`}</span>
-                      );
-                    })
+                    return (
+                      <span
+                        className={styles.category}
+                        key={cat}
+                      >{`${cat}`}</span>
+                    );
+                  })
                   : ""}
               </p>
             </article>
